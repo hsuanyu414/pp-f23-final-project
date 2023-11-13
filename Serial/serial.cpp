d5719450eeef32efa118cf7e9fad66a7f24f3874
hsuanyu414 <93172849+hsuanyu414@users.noreply.github.com>
2023-11-13 23:57:24 +0800

    refactor: conv function param added
    1. 新增全域變數 width, height
    2. 修改 conv 參數以支援 pthread 等

--- Serial/serial.cpp
+++ Serial/serial.cpp
@@ -6,13 +6,22 @@
 
 using namespace std;
 
-void conv(uint8_t *input, float *kernel, uint8_t *output, int width, int height, int kernel_size){
+int width, height;
+
+void conv(
+    uint8_t *input, 
+    float *kernel, 
+    uint8_t *output, 
+    int start_width, int end_width, 
+    int start_height, int end_height, 
+    int kernel_size){
+
     float temp_pixel = 0;
     printf("convolution start!\n");
     // TODO: boundary check due to padding, modify to the version without padding
     int indexi, indexj;
-    for(int i = 0 ; i < height ; i++){
-        for(int j = 0; j < width ; j++){
+    for(int i = start_height ; i < end_height ; i++){
+        for(int j = start_width; j < end_width ; j++){
             temp_pixel = 0;
             for(int k = 0 ; k < kernel_size ; k++){
                 for(int l = 0 ; l < kernel_size ; l++){
@@ -24,6 +33,12 @@ void conv(uint8_t *input, float *kernel, uint8_t *output, int width, int height,
                         temp_pixel += float(input[(indexi) * width + (indexj)] * kernel[k * kernel_size + l]);
                 }
             }
+            // to clamp the value to 0~255 to prevent overflow
+            if(temp_pixel < 0)
+                temp_pixel = 0;
+            else if(temp_pixel > 255)
+                temp_pixel = 255;
+            // return the result back to output
             output[i * width + j] = uint8_t(temp_pixel);
         }
     }
@@ -39,46 +54,49 @@ int main(){
     // read the header
     sBmpHeader header = {0};
     fread(&header, sizeof(sBmpHeader), 1, fp);
+    width = header.width;
+    height = header.height;
     // print_bmp_header(&header);
 
     fseek(fp, header.offset, SEEK_SET);
     // read each pixel (for 24-bit bmp)
-    pixel24 *p = (pixel24 *)malloc(sizeof(pixel24) * header.width * header.height);
-    fread(p, sizeof(pixel24), header.width * header.height, fp);
+    pixel24 *p = (pixel24 *)malloc(sizeof(pixel24) * width * height);
+    fread(p, sizeof(pixel24), width * height, fp);
     fclose(fp);
 
     // to one dimention gray
-    uint8_t *p1_gray = (uint8_t *)malloc(sizeof(uint8_t) * (header.width) * (header.height));
-    for(int i = 0 ; i < (header.width) * (header.height) ; i ++){
+    uint8_t *p1_gray = (uint8_t *)malloc(sizeof(uint8_t) * (width) * (height));
+    for(int i = 0 ; i < (width) * (height) ; i ++){
         p1_gray[i] = 0;
     }
-    for(int i = 0 ; i < header.height; i++){
-        for(int j = 0; j < header.width; j++){
-            int gray = (p[i * (header.width) + j].r + p[i * (header.width) + j].g + p[i * (header.width) + j].b) / 3;
-            p1_gray[i * (header.width ) + j] = gray;
+    for(int i = 0 ; i < height; i++){
+        for(int j = 0; j < width; j++){
+            int gray = (p[i * (width) + j].r + p[i * (width) + j].g + p[i * (width) + j].b) / 3;
+            p1_gray[i * (width ) + j] = gray;
         }
     }
     
     // convolution
-    uint8_t *p1_gray_after_gaussian = (uint8_t *)malloc(sizeof(uint8_t) * (header.width) * (header.height));
+    uint8_t *p1_gray_after_gaussian = (uint8_t *)malloc(sizeof(uint8_t) * (width) * (height));
     
-    for(int i = 0 ; i < (header.width) * (header.height) ; i ++){
+    for(int i = 0 ; i < (width) * (height) ; i ++){
         p1_gray_after_gaussian[i] = 0;
     }
     
     float gaussian_kernel[9] = {1.0/16, 2.0/16, 1.0/16, 2.0/16, 4.0/16, 2.0/16, 1.0/16, 2.0/16, 1.0/16};
-    conv(p1_gray, gaussian_kernel, p1_gray_after_gaussian, header.width , header.height , 3);
-
-
+    conv(p1_gray, gaussian_kernel, p1_gray_after_gaussian, 0, width/2, 0, height/2, 3);
+    conv(p1_gray, gaussian_kernel, p1_gray_after_gaussian, width/2, width, 0, height/2, 3);
+    conv(p1_gray, gaussian_kernel, p1_gray_after_gaussian, width/2, width, width/2, height, 3);
+    conv(p1_gray, gaussian_kernel, p1_gray_after_gaussian, 0, width/2, width/2, height, 3);
     uint8_t *final_result = p1_gray_after_gaussian;
 
     // back to three dimention gray
-    pixel24 *p1 = (pixel24 *)malloc(sizeof(pixel24) * header.width * header.height);
-    for(int i = 0 ; i < header.height; i++){
-        for(int j = 0; j < header.width; j++){
-            p1[i * header.width + j].r = final_result[i * (header.width ) + j];
-            p1[i * header.width + j].g = final_result[i * (header.width ) + j];
-            p1[i * header.width + j].b = final_result[i * (header.width ) + j];
+    pixel24 *p1 = (pixel24 *)malloc(sizeof(pixel24) * width * height);
+    for(int i = 0 ; i < height; i++){
+        for(int j = 0; j < width; j++){
+            p1[i * width + j].r = final_result[i * (width ) + j];
+            p1[i * width + j].g = final_result[i * (width ) + j];
+            p1[i * width + j].b = final_result[i * (width ) + j];
         }
     }
 
@@ -90,7 +108,7 @@ int main(){
         exit(1);
     }
     fwrite(&header, sizeof(sBmpHeader), 1, fp2);
-    fwrite(p1, sizeof(pixel24), header.width * header.height, fp2);
+    fwrite(p1, sizeof(pixel24), width * height, fp2);
     fclose(fp2);
     free(p);
     return 0;
